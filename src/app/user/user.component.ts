a72f1989d84c1dae38461b2fcc2ea0662492e023
Shirley <shirley.pham@terralogic.com>
2025-05-21 16:44:39 +0700

    210525: typescript: type aliases & interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,23 @@
-import { Component, computed, EventEmitter, Input, Output, output } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  Output,
+  output,
+} from '@angular/core';
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -7,14 +26,9 @@ import { Component, computed, EventEmitter, Input, Output, output } from '@angul
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
-  
   // imagePath = computed(()=> {
   //   return 'assets/users/' + this.avatar();
   // })
