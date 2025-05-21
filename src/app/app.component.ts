39d6f4ae98e35d0709ddb6f08fb7f8ec14e60a01
Pham Anh Thi <shirley.pham@terralogicvietnam.com>
2025-05-21 08:50:27 +0700

    210525: update old lesson of signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,5 @@ import { UserComponent } from './user/user.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-course-2025';
+  users = DUMMY_USERS;
 }
