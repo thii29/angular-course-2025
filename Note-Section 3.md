10f9dfb591acb4cb116f685748568578bb9925a5
Thi Pham <thipham1123@gmail.com>
2025-05-25 14:51:42 +0700

    🌱250525: declare & use components

---
+++ Note-Section 3.md
@@ -0,0 +1,3 @@
+## Section 3
+- Every Angular application when using Angular modules needs that BrowserModule.
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -7,6 +8,6 @@ import { UserComponent } from './user/user.component';
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppModule {}
