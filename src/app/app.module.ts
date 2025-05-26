f3635bf6b062e6c6dab81c3f132e5956e0cba274
Shirley <shirley.pham@terralogic.com>
2025-05-26 08:53:23 +0700

    260525: creating & using shared Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CardComponent } from './shared/card/card.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -14,12 +14,11 @@ import { UserComponent } from './user/user.component';
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     NewTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
