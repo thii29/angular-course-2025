b6d79f551a2a89574a34bfc7d60a7d3f0b5ee134
Shirley <shirley.pham@terralogic.com>
2025-05-26 10:08:40 +0700

    260525: creating more complex module-based app structures

--- Note-Section 3.md
+++ Note-Section 3.md
@@ -1,3 +1,4 @@
 ## Section 3
 - Every Angular application when using Angular modules needs that BrowserModule.
 - FormModule is such as a NgModule
+- BrowserModule is a special module that's only meant to be imported into the root module with which you bootstrap the application.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
