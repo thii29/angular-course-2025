de645d0f6dbea22cba912c0262d4d42155a3fd7b
Thi Pham <thipham1123@gmail.com>
2025-05-28 20:53:49 +0700

    🌱280525: property binding: repetition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ServerStatusComponent } from './components/dashboard/server-status/server-status.component';
 import { TrafficComponent } from './components/dashboard/traffic/traffic.component';
 import { TicketsComponent } from './components/dashboard/tickets/tickets.component';
+import { DashboardItemComponent } from "./components/dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ import { TicketsComponent } from './components/dashboard/tickets/tickets.compone
     ServerStatusComponent,
     TrafficComponent,
     TicketsComponent,
-  ],
+    DashboardItemComponent
+],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- src/app/components/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/components/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
-
+  @Input({ required: true }) image!: { src: string; alt: string };
+  @Input({ required: true }) title!: string;
 }
