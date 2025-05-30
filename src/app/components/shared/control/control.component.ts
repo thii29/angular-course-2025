6f51102cac1811655bcfe86b9956c3880ddee819
Shirley <shirley.pham@terralogic.com>
2025-05-30 10:31:18 +0700

    300525: interacting with host elements via @HostListener & @HostBinding

--- src/app/components/shared/control/control.component.ts
+++ src/app/components/shared/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,8 +15,17 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    // '(click)': 'onClick()'
   },
 })
 export class ControlComponent {
+  //@HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('clicked!');
+  // }
   label = input.required<string>();
+
+  onClick(){
+    console.log("clicked!")
+  }
 }
