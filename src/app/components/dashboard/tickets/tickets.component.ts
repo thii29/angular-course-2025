ec965a15aa79de11d05f89ede1d510a2fa669c72
Shirley <shirley.pham@terralogic.com>
2025-06-02 15:27:34 +0700

    020625: a closer look at template for loops

--- src/app/components/dashboard/tickets/tickets.component.ts
+++ src/app/components/dashboard/tickets/tickets.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { Ticket } from './ticket.model';
 import { tick } from '@angular/core/testing';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
