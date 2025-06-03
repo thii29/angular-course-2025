18093cc7022d47d27628375c5524503bd13d324a
Shirley <shirley.pham@terralogic.com>
2025-06-03 09:18:53 +0700

    030625: initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { AuthComponent } from './auth/auth.component';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [AuthComponent, LearningResourcesComponent],
 })
-export class AppComponent {
-  title = 'angular-course-2025';
-}
+export class AppComponent {}
