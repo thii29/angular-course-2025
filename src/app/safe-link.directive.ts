1d5a7ecb219bd37d2a881aef72f3ef25bad1a784
Shirley <shirley.pham@terralogic.com>
2025-06-16 09:54:33 +0700

    160625: directives & dependency injection

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,7 +8,8 @@ import { Directive, input } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
-  queryParam = input('myapp', {alias: 'appSafeLink'});
+  queryParam = input('myapp', { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
   constructor() {
     console.log('SafeLink Directive is active!');
@@ -17,8 +18,8 @@ export class SafeLinkDirective {
   onConfirmLeavePage(event: MouseEvent) {
     const wantsToLeave = window.confirm('Do you want to leave the app?');
     if (wantsToLeave) {
-      const address = (event.target as HTMLAnchorElement).href;
-      (event.target as HTMLAnchorElement).href =
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href =
         address + '?from=' + this.queryParam();
       return;
     }
