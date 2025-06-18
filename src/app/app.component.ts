fb4ca49869d29158c874c191d552d223a4033a11
Thi Pham <thipham1123@gmail.com>
2025-06-18 21:52:41 +0700

    🌱180625: initial project lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [TasksComponent],
 })
-export class AppComponent {
-  title = 'angular-course-2025';
-}
+export class AppComponent {}
